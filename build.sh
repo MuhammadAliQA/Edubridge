1d351a5fe22a5eb19d745936edddf6b217dcc8ef
User Userov <user@gmail.com>
2026-03-19 01:54:44 +0500

    fix python version + build

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit
 
+echo "Installing requirements..."
+pip install --upgrade pip
 pip install -r requirements.txt
+
+echo "Collect static..."
 python manage.py collectstatic --no-input
-python manage.py migrate
+
+echo "Migrate..."
+python manage.py migrate --noinput
